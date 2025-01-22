190b20646642cec8c417f06d894c99563f20935e
Eirik L. Vullum <evullum@gmail.com>
2025-01-22 23:38:19 +0100

    Add support for mutations + test

--- src/lib/query.test.ts
+++ src/lib/query.test.ts
@@ -1,10 +1,12 @@
 import { signal } from "@preact/signals-core";
-import { query, queryCache } from "./query";
+import { query, getQueryClient, mutation } from "./query";
+import { sleep } from "./utils";
 
 describe("query", () => {
   beforeEach(() => {
     vi.useFakeTimers();
-    queryCache.clear();
+    getQueryClient().getQueryCache().clear();
+    getQueryClient().getMutationCache().clear();
   });
 
   it("should initialize and resolve correctly (promise)", async () => {
@@ -93,6 +95,94 @@ describe("query", () => {
     expect(myQuery.isLoading.value).toBe(false);
     expect(myQuery.isPending.value).toBe(false);
     expect(myQuery.data.value).toEqual(20);
+    expect(myQuery.error.value).toEqual(null);
+  });
+});
+
+describe("mutation", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+    getQueryClient().getQueryCache().clear();
+    getQueryClient().getMutationCache().clear();
+  });
+
+  it("should trigger re-fetching of query when mutation invalidates cache", async () => {
+    let count = 10;
+    const getNextCount = () => count++;
+
+    const queryKey = ["abc"];
+
+    const myQuery = query(() => ({
+      queryKey,
+      queryFn: async () => {
+        await sleep(100);
+        return getNextCount();
+      },
+    }));
+
+    expect(myQuery.isLoading.value).toBe(true);
+    expect(myQuery.isPending.value).toBe(true);
+    expect(myQuery.data.value).toEqual(undefined);
+    expect(myQuery.error.value).toEqual(null);
+
+    await vi.advanceTimersToNextTimerAsync();
+
+    expect(myQuery.isLoading.value).toBe(false);
+    expect(myQuery.isPending.value).toBe(false);
+    expect(myQuery.data.value).toEqual(10);
+    expect(myQuery.error.value).toEqual(null);
+
+    const myMutation = mutation(() => ({
+      mutationFn: async () => {
+        await sleep(100);
+        return 50;
+      },
+      onSuccess: () => {
+        console.log("success");
+        getQueryClient().invalidateQueries({ queryKey });
+      },
+    }));
+
+    myMutation.mutate.value({});
+
+    expect(myMutation.isSuccess.value).toBe(false);
+    expect(myMutation.isPending.value).toBe(true);
+    expect(myMutation.data.value).toEqual(undefined);
+    expect(myMutation.error.value).toEqual(null);
+
+    expect(myQuery.isFetching.value).toBe(false);
+    expect(myQuery.isSuccess.value).toBe(true);
+    expect(myQuery.isLoading.value).toBe(false);
+    expect(myQuery.isPending.value).toBe(false);
+    expect(myQuery.data.value).toEqual(10);
+    expect(myQuery.error.value).toEqual(null);
+
+    await vi.advanceTimersToNextTimerAsync();
+
+    expect(myMutation.isSuccess.value).toBe(true);
+    expect(myMutation.isPending.value).toBe(false);
+    expect(myMutation.data.value).toEqual(50);
+    expect(myMutation.error.value).toEqual(null);
+
+    expect(myQuery.isFetching.value).toBe(true);
+    expect(myQuery.isSuccess.value).toBe(true);
+    expect(myQuery.isLoading.value).toBe(false);
+    expect(myQuery.isPending.value).toBe(false);
+    expect(myQuery.data.value).toEqual(10);
+    expect(myQuery.error.value).toEqual(null);
+
+    await vi.advanceTimersToNextTimerAsync();
+
+    expect(myMutation.isSuccess.value).toBe(true);
+    expect(myMutation.isPending.value).toBe(false);
+    expect(myMutation.data.value).toEqual(50);
+    expect(myMutation.error.value).toEqual(null);
+
+    expect(myQuery.isFetching.value).toBe(false);
+    expect(myQuery.isSuccess.value).toBe(true);
+    expect(myQuery.isLoading.value).toBe(false);
+    expect(myQuery.isPending.value).toBe(false);
+    expect(myQuery.data.value).toEqual(11);
     expect(myQuery.error.value).toEqual(null);
   });
 });
--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,36 +1,67 @@
 import { batch, computed, effect, Signal, signal } from "@preact/signals-core";
-import { derived } from "./signals";
-import { QueryClient, QueryCache, QueryObserver } from "@tanstack/query-core";
+import {
+  MutationObserver,
+  QueryClient,
+  QueryObserver,
+} from "@tanstack/query-core";
 
 /**
- * Create query client and cache
- */
-export const queryClient: QueryClient = new QueryClient();
-export const queryCache: QueryCache = queryClient.getQueryCache();
-
-/**
- * Mount query client
- */
-queryClient.mount();
-
-/**
- * NOTE: This is fake for now
- * (does not do any caching, but emulates a simple query interface)
+ * Simplified query config type
  */
 export type QueryConfig<T> = {
   queryKey: string[];
   queryFn: () => Promise<T>;
 };
 
+/**
+ * Simplified mutation config type
+ */
+export type MutationConfig<T, U> = {
+  queryKey?: string[];
+  mutationFn: (variables: U) => Promise<T>;
+  onSuccess?: () => void;
+  onError?: () => void;
+};
+
+/**
+ * Create default query client
+ */
+const activeQueryClient: Signal<QueryClient> = signal(new QueryClient());
+
+/**
+ * Mount default query client
+ */
+activeQueryClient.value.mount();
+
+/**
+ * Export setter for query client
+ */
+export const setQueryClient = (newQueryClient: QueryClient) => {
+  activeQueryClient.value.unmount();
+  activeQueryClient.value = newQueryClient;
+  activeQueryClient.value.mount();
+};
+
+/**
+ * Export getter for query client
+ */
+export const getQueryClient = () => activeQueryClient.value;
+
+/**
+ * NOTE: This is a simplified version of the tanstack-query query interface
+ */
 export function query<T>(getQueryConfig: () => QueryConfig<T>) {
+  const queryClient = computed(() => getQueryClient());
   const queryConfig = computed(() => getQueryConfig());
   const queryObserver = computed(
-    () => new QueryObserver(queryClient, queryConfig.value),
+    () => new QueryObserver(queryClient.value, queryConfig.value),
   );
 
   const data: Signal<T | undefined> = signal(undefined);
   const isLoading: Signal<boolean> = signal(false);
   const isPending: Signal<boolean> = signal(false);
+  const isFetching: Signal<boolean> = signal(false);
+  const isSuccess: Signal<boolean> = signal(false);
   const error: Signal<unknown> = signal(null);
 
   effect(() => {
@@ -39,6 +70,8 @@ export function query<T>(getQueryConfig: () => QueryConfig<T>) {
         data.value = result.data;
         isLoading.value = result.isLoading;
         isPending.value = result.isPending;
+        isFetching.value = result.isFetching;
+        isSuccess.value = result.isSuccess;
         error.value = result.error;
       });
     });
@@ -48,19 +81,42 @@ export function query<T>(getQueryConfig: () => QueryConfig<T>) {
     };
   });
 
-  return { data, isLoading, isPending, error };
+  return { data, isLoading, isPending, isFetching, isSuccess, error };
 }
 
 /**
- * NOTE: This is fake for now
- * (does not do any cache invalidation, but emulates a simple mutation interface)
+ * NOTE: This is a simplified version of the tanstack-query mutation interface
  */
-export type MutationConfig<T> = {
-  queryKey: string[];
-  mutationFn: () => Promise<T>;
-};
-
-export function mutation<T>(getMutationConfig: () => MutationConfig<T>) {
+export function mutation<T, U>(getMutationConfig: () => MutationConfig<T, U>) {
+  const queryClient = computed(() => getQueryClient());
   const mutationConfig = computed(() => getMutationConfig());
-  return derived(() => mutationConfig.value.mutationFn());
+  const mutationObserver = computed(
+    () => new MutationObserver(queryClient.value, mutationConfig.value),
+  );
+
+  const data: Signal<T | undefined> = signal(undefined);
+  const isSuccess: Signal<boolean> = signal(false);
+  const isPending: Signal<boolean> = signal(false);
+  const error: Signal<unknown> = signal(null);
+
+  const mutate = computed(() => mutationObserver.value.mutate);
+
+  effect(() => {
+    const disposeMutationSubscription = mutationObserver.value.subscribe(
+      (result) => {
+        batch(() => {
+          data.value = result.data;
+          isSuccess.value = result.isSuccess;
+          isPending.value = result.isPending;
+          error.value = result.error;
+        });
+      },
+    );
+
+    return () => {
+      disposeMutationSubscription();
+    };
+  });
+
+  return { mutate, data, isSuccess, isPending, error };
 }
