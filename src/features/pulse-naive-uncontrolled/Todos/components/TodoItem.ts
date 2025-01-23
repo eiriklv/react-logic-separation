d6cadcb98eacd44805240e1452da92c3799bdcc3
Eirik Vullum <evullum@gmail.com>
2025-01-23 16:12:37 +0100

    Cleanup

--- src/features/pulse-naive-uncontrolled/Todos/components/TodoItem.ts
+++ src/features/pulse-naive-uncontrolled/Todos/components/TodoItem.ts
@@ -1,4 +1,4 @@
-import { BaseElement, container, divider, text } from "@cognite/pulse";
+import { BaseElement, text } from "@cognite/pulse";
 import { Todo } from "../types";
 
 type Props = {
@@ -6,5 +6,5 @@ type Props = {
 };
 
 export function TodoItem({ todo }: Props): BaseElement {
-  return container().addChildren(text(todo.text).id(todo.id), divider());
+  return text(todo.text).id(todo.id);
 }
