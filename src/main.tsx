453deb6cb5573c54c94af6f1d51711a12fd3afaf
Eirik Vullum <evullum@gmail.com>
2025-02-11 00:30:40 +0100

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Talk from "./Talk.tsx";
+import Talk from "./separation-of-concerns-talk/Talk.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
