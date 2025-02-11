65cd70eab8ab0a0dabb79aeafaebcc777499b629
Eirik Vullum <evullum@gmail.com>
2025-02-11 12:53:18 +0100

    Cleanup

--- src/separation-of-concerns-talk/2-add-e2e-test-for-happy-path/components/ReminderItem.tsx
+++
@@ -1,9 +0,0 @@
-import { Reminder } from "../types";
-
-type Props = {
-  reminder: Reminder;
-};
-
-export function ReminderItem({ reminder }: Props) {
-  return <li>{reminder.text}</li>;
-}
--- src/separation-of-concerns-talk/2-add-e2e-test-for-happy-path/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Reminder {
-  id: string;
-  text: string;
-}
