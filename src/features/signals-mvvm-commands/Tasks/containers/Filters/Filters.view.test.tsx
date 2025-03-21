833d12f5c82aa045ca67db4b6664c6287e52c1bf
Eirik Vullum <evullum@gmail.com>
2025-03-21 18:14:30 +0100

    Add initial tests for Filters

---
+++ src/features/signals-mvvm-commands/Tasks/containers/Filters/Filters.view.test.tsx
@@ -0,0 +1,83 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Filters } from "./Filters.view";
+import {
+  FiltersContext,
+  FiltersContextInterface,
+} from "./Filters.view.context";
+
+describe("Filters Component", () => {
+  it("Renders correctly", () => {
+    // arrange
+    const dependencies: FiltersContextInterface = {
+      useFiltersViewModel: vi.fn(() => ({
+        users: [
+          {
+            id: "user-1",
+            name: "John Doe",
+            profileImageUrl: "./src/test.png",
+          },
+          {
+            id: "user-2",
+            name: "Jane Doe",
+            profileImageUrl: "./src/test-2.png",
+          },
+        ],
+        selectedOwnerId: "user-1",
+        setSelectedOwnerId: vi.fn(),
+      })),
+    };
+
+    render(
+      <FiltersContext.Provider value={dependencies}>
+        <Filters />
+      </FiltersContext.Provider>,
+    );
+
+    // assert
+    expect(screen.getByText("Filters")).toBeInTheDocument();
+    expect(screen.getByText(/John Doe/)).toBeInTheDocument();
+    expect(screen.getByText(/Jane Doe/)).toBeInTheDocument();
+  });
+
+  it("Calls correct handler with correct arguments when selecting an owner", async () => {
+    // arrange
+    const setSelectedOwnerId = vi.fn();
+
+    const dependencies: FiltersContextInterface = {
+      useFiltersViewModel: vi.fn(() => ({
+        users: [
+          {
+            id: "user-1",
+            name: "John Doe",
+            profileImageUrl: "./src/test.png",
+          },
+          {
+            id: "user-2",
+            name: "Jane Doe",
+            profileImageUrl: "./src/test-2.png",
+          },
+        ],
+        selectedOwnerId: "",
+        setSelectedOwnerId,
+      })),
+    };
+
+    render(
+      <FiltersContext.Provider value={dependencies}>
+        <Filters />
+      </FiltersContext.Provider>,
+    );
+
+    // assert
+    expect(screen.getByText("Filters")).toBeInTheDocument();
+    expect(screen.getByText(/John Doe/)).toBeInTheDocument();
+    expect(screen.getByText(/Jane Doe/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(screen.getByRole("combobox"), "John Doe");
+
+    // assert
+    expect(setSelectedOwnerId).toHaveBeenCalledWith("user-1");
+  });
+});
