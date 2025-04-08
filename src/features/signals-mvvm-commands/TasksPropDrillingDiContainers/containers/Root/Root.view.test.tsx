3c0f0c7a79c021d93f7ebc1a9186ea3954460b39
Eirik Vullum <evullum@gmail.com>
2025-04-08 19:25:04 +0200

    Add Root view test

---
+++ src/features/signals-mvvm-commands/TasksPropDrillingDiContainers/containers/Root/Root.view.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Root, RootDependencies } from "./Root.view";
+import { useRootViewModel } from "./Root.view-model";
+import { QueryClient } from "@tanstack/query-core";
+
+describe("Root", () => {
+  it("Renders correctly", () => {
+    // arrange
+    const dependencies: RootDependencies = {
+      useRootViewModel: () =>
+        ({
+          commands: {},
+          queryClient: new QueryClient(),
+        }) as ReturnType<typeof useRootViewModel>,
+      App: () => <div data-testid="App" />,
+    };
+
+    render(<Root dependencies={dependencies} />);
+
+    // assert
+    expect(screen.getByTestId("App")).toBeInTheDocument();
+  });
+});
