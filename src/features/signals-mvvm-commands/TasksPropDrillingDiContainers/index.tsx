492149d7744238db17fb2e91bc86c3363bb0b018
Eirik Vullum <evullum@gmail.com>
2025-04-08 19:55:34 +0200

    Cleanup

--- src/features/signals-mvvm-commands/TasksPropDrillingDiContainers/index.tsx
+++ src/features/signals-mvvm-commands/TasksPropDrillingDiContainers/index.tsx
@@ -1,13 +1,7 @@
 import { Root } from "./containers/Root/Root.view";
 
 /**
- * TODO: Here is where the global providers
- * need to be initialized (potentially in the view model of this)
- *
- * - Models
- * - Commands
- * - Flags
- * - Etc..
+ * Entry point to the feature
  */
 export function TasksFeature() {
   return <Root />;
