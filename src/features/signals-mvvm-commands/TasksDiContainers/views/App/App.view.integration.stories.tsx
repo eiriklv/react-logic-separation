5aa40da94371a41ebec7871c1ff2b66a8057db9f
Eirik Vullum <evullum@gmail.com>
2025-04-10 22:50:06 +0200

    Cleanup

--- src/features/signals-mvvm-commands/TasksDiContainers/views/App/App.view.integration.stories.tsx
+++ src/features/signals-mvvm-commands/TasksDiContainers/views/App/App.view.integration.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { App } from "./App.view";
 import { Task, User } from "../../types";
-import { ComponentType } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import {
   CommandsContext,
@@ -10,65 +9,52 @@ import {
 import { createProviderTree } from "../../../../../lib/create-provider-tree";
 import { createQueryClient } from "../../utils/create-query-client";
 
-type Props = {
-  children?: JSX.Element;
-};
-
-const Container: ComponentType<Props> = ({ children }) => {
-  return <>{children}</>;
-};
-
 const meta = {
-  component: Container,
-  decorators: [
-    (Story, { parameters }) => {
-      // create query client for test
-      const queryClient = createQueryClient();
+  render: (_, { parameters }) => {
+    // create query client for test
+    const queryClient = createQueryClient();
 
-      // create mock tasks
-      const mockTasks: Task[] = parameters.tasks;
+    // create mock tasks
+    const mockTasks: Task[] = parameters.tasks;
 
-      // create mock users
-      const mockUsers: User[] = parameters.users;
+    // create mock users
+    const mockUsers: User[] = parameters.users;
 
-      // create mock commands
-      const commands: CommandsContextInterface = {
-        listTasksCommand: async () => mockTasks,
-        addTaskCommand: async () => ({
-          id: "1",
-          text: "task",
-          ownerId: "user-1",
-        }),
-        deleteTaskCommand: async () => {},
-        getUserCommand: async (userId) => {
-          return mockUsers.find((user) => user.id === userId);
-        },
-        listUsersCommand: async () => mockUsers,
-      };
+    // create mock commands
+    const commands: CommandsContextInterface = {
+      listTasksCommand: async () => mockTasks,
+      addTaskCommand: async () => ({
+        id: "1",
+        text: "task",
+        ownerId: "user-1",
+      }),
+      deleteTaskCommand: async () => {},
+      getUserCommand: async (userId) => {
+        return mockUsers.find((user) => user.id === userId);
+      },
+      listUsersCommand: async () => mockUsers,
+    };
 
-      /**
-       * Create provider tree
-       */
-      const Providers = createProviderTree([
-        <QueryClientProvider client={queryClient} />,
-        <CommandsContext.Provider value={commands} />,
-      ]);
+    /**
+     * Create provider tree
+     */
+    const Providers = createProviderTree([
+      <QueryClientProvider client={queryClient} />,
+      <CommandsContext.Provider value={commands} />,
+    ]);
 
-      /**
-       * Render a version that injects all the dependencies
-       * we created further up so that we can test our integration
-       */
-      return (
-        <Story>
-          <Providers>
-            <App />
-          </Providers>
-        </Story>
-      );
-    },
-  ],
+    /**
+     * Render a version that injects all the dependencies
+     * we created further up so that we can test our integration
+     */
+    return (
+      <Providers>
+        <App />
+      </Providers>
+    );
+  },
   tags: ["autodocs"],
-} satisfies Meta<typeof Container>;
+} satisfies Meta<typeof App>;
 
 export default meta;
 
@@ -90,7 +76,7 @@ export const Default: Story = {
   },
 };
 
-export const NoAppNoUsers: Story = {
+export const NoTasksNoUsers: Story = {
   args: {},
   parameters: {
     tasks: [] satisfies Task[],
--- src/features/signals-mvvm-commands/TasksPropDrillingDiContainers/views/App/App.view.integration.stories.tsx
+++ src/features/signals-mvvm-commands/TasksPropDrillingDiContainers/views/App/App.view.integration.stories.tsx
@@ -2,7 +2,6 @@ import { Meta, StoryObj } from "@storybook/react";
 import { App } from "./App.view";
 import { QueryClient } from "@tanstack/query-core";
 import { Task, User } from "../../types";
-import { ComponentType } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import {
   CommandsContext,
@@ -10,66 +9,52 @@ import {
 } from "../../providers/commands.provider";
 import { createProviderTree } from "../../../../../lib/create-provider-tree";
 
-type Props = {
-  children?: JSX.Element;
-};
-
-const Container: ComponentType<Props> = ({ children }) => {
-  return <>{children}</>;
-};
-
 const meta = {
-  component: Container,
-  title: "Tasks",
-  decorators: [
-    (Story, { parameters }) => {
-      // create query client for test
-      const queryClient = new QueryClient();
+  render: (_, { parameters }) => {
+    // create query client for test
+    const queryClient = new QueryClient();
 
-      // create mock tasks
-      const mockTasks: Task[] = parameters.tasks;
+    // create mock tasks
+    const mockTasks: Task[] = parameters.tasks;
 
-      // create mock users
-      const mockUsers: User[] = parameters.users;
+    // create mock users
+    const mockUsers: User[] = parameters.users;
 
-      // create mock commands
-      const commands: CommandsContextInterface = {
-        listTasksCommand: async () => mockTasks,
-        addTaskCommand: async () => ({
-          id: "1",
-          text: "task",
-          ownerId: "user-1",
-        }),
-        deleteTaskCommand: async () => {},
-        getUserCommand: async (userId) => {
-          return mockUsers.find((user) => user.id === userId);
-        },
-        listUsersCommand: async () => mockUsers,
-      };
+    // create mock commands
+    const commands: CommandsContextInterface = {
+      listTasksCommand: async () => mockTasks,
+      addTaskCommand: async () => ({
+        id: "1",
+        text: "task",
+        ownerId: "user-1",
+      }),
+      deleteTaskCommand: async () => {},
+      getUserCommand: async (userId) => {
+        return mockUsers.find((user) => user.id === userId);
+      },
+      listUsersCommand: async () => mockUsers,
+    };
 
-      /**
-       * Create provider tree
-       */
-      const Providers = createProviderTree([
-        <QueryClientProvider client={queryClient} />,
-        <CommandsContext.Provider value={commands} />,
-      ]);
+    /**
+     * Create provider tree
+     */
+    const Providers = createProviderTree([
+      <QueryClientProvider client={queryClient} />,
+      <CommandsContext.Provider value={commands} />,
+    ]);
 
-      /**
-       * Render a version that injects all the dependencies
-       * we created further up so that we can test our integration
-       */
-      return (
-        <Story>
-          <Providers>
-            <App />
-          </Providers>
-        </Story>
-      );
-    },
-  ],
+    /**
+     * Render a version that injects all the dependencies
+     * we created further up so that we can test our integration
+     */
+    return (
+      <Providers>
+        <App />
+      </Providers>
+    );
+  },
   tags: ["autodocs"],
-} satisfies Meta<typeof Container>;
+} satisfies Meta<typeof App>;
 
 export default meta;
 
