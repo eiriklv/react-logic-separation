d211aee3c51538cca4ba0d868ac29ec7ee6befe4
Eirik L. Vullum <evullum@gmail.com>
2025-08-30 20:32:17 +0200

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Tasks } from "./features/signals-mvvm-commands/TasksPropDrilling/Tasks.view.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Tasks />
+    <App />
   </StrictMode>,
 );
