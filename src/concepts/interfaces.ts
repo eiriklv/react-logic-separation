4d3e70781996af588d6961b5c84f786f00338dd0
Eirik L. Vullum <evullum@gmail.com>
2025-09-22 21:14:21 +0200

    Cleanup

--- src/concepts/interfaces.ts
+++ src/concepts/interfaces.ts
@@ -61,11 +61,11 @@ ten(); // fails type check
 ten(2, 3); // 10
 
 /**
- * Let's declare a compount interface
+ * Let's declare a compound interface
  */
 interface MathOperations {
   add: NumberOperationWithTwoOperands;
-  multiply: NumberOperationWithTwoOperands;
+  double: NumberOperationWithTwoOperands;
   zero: NumberOperationWithTwoOperands;
 }
 
@@ -80,7 +80,7 @@ interface MathOperations {
  */
 const math: MathOperations = {
   add: () => 5,
-  multiply: () => 5,
+  double: () => 5,
   zero: () => 0,
 };
 
@@ -89,8 +89,8 @@ const math: MathOperations = {
  */
 math.add(); // fails type check
 math.add(2, 3); // 5
-math.multiply(); // fails type check
-math.multiply(2, 3); // 5
+math.double(); // fails type check
+math.double(2, 3); // 4
 math.zero(); // fails type check
 math.zero(2, 3); // 0
 
@@ -114,7 +114,7 @@ interface NumberOperationNoOperands {
  * - the new one with no operands
  */
 const myAdd: NumberOperationWithTwoOperands = (a, b) => a + b;
-const myMultiplyBy10: NumberOperationWithOneOperand = (a) => a * 10;
+const myDouble: NumberOperationWithOneOperand = (a) => a * 2;
 const myZero: NumberOperationNoOperands = () => 0;
 
 /**
@@ -123,32 +123,80 @@ const myZero: NumberOperationNoOperands = () => 0;
  * have to provide extra operands that will just be discarded anyway
  */
 myAdd(2, 3); // 5
-myMultiplyBy10(2); // 20
+myDouble(2); // 4
 myZero(); // 0
+
+/**
+ * Let's declare an object containing this collection
+ * but without tying it to a compound interface
+ */
+const myMathStuff = {
+  add: myAdd,
+  double: myDouble,
+  zero: myZero,
+};
+
+/**
+ * Using them through the object works just the same
+ * as using the individually
+ */
+myMathStuff.add(2, 3);
+myMathStuff.double(2);
+myMathStuff.zero();
 
 /**
  * Now let's declare another implementation
  * of the compound interface, where
- * we use the above implementations
+ * we use the above implementation
  *
  * This is completely fine, because the
  * subset interfaces completely overlaps
  * with the original one with two operands
  *
- * You might look at it as the subset
- * interfaces being cast into the complete one
+ * You might look at it as our
+ * object being cast into the compound type
  */
-const otherMath: MathOperations = {
-  add: myAdd,
-  multiply: myMultiplyBy10,
-  zero: myZero,
-};
+const otherMath: MathOperations = myMathStuff;
 
 /**
- * But it still requires two operands to be passed for all of them
+ * But now it requires two operands to be passed for all of them
  * because through the compound interface they are all seen as
  * the original interface that requires two operands
  */
-otherMath.add();
-otherMath.multiply();
-otherMath.zero();
+otherMath.add(2, 3); // 5
+otherMath.double(2); // fails type check
+otherMath.double(2, 3); // 4
+otherMath.zero(); // fails type check
+otherMath.zero(2, 3); // 0
+
+/**
+ * Now let's declare another compound interface,
+ * which is narrower than the original one
+ */
+interface MyMathDependencies {
+  add: (a: number, b: number) => number;
+  double: (a: number) => number;
+  zero: () => number;
+}
+
+/**
+ * If we try to map the 'otherMath' object into this
+ * new compound interface we won't be allowed to,
+ * as that is a stricter interface than the one
+ * declared above (not fully overlapping)
+ */
+const mathDependencies: MyMathDependencies = otherMath;
+
+/**
+ * But if we try to map the 'myMathStuff' object
+ * into the same interface that's completely fine,
+ * as it fully overlaps
+ */
+const otherMathDependencies: MyMathDependencies = myMathStuff;
+
+/**
+ * Use the methods through the compound interface
+ */
+otherMathDependencies.add(2, 3); // 5
+otherMathDependencies.double(2); // 4
+otherMathDependencies.zero(); // 0
