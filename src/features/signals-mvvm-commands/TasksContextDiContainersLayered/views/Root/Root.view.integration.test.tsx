0f0b9c90794a07698cf89f84d0202fb54279bef4
Eirik Vullum <evullum@gmail.com>
2025-10-31 15:57:28 +0100

    Add example of integration test using the service layer

--- src/features/signals-mvvm-commands/TasksContextDiContainersLayered/views/Root/Root.view.integration.test.tsx
+++ src/features/signals-mvvm-commands/TasksContextDiContainersLayered/views/Root/Root.view.integration.test.tsx
@@ -12,15 +12,13 @@ import { IUsersService } from "../../services/users.service";
 import { createQueryClient } from "../../utils/create-query-client";
 import { RootViewModelDependencies } from "./Root.view-model.dependencies";
 import { createProviderTree } from "../../../../../lib/create-provider-tree";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { RootViewModelContext } from "./Root.view-model.context";
 import { RootContext } from "./Root.view.context";
 
-describe("Root Integration (view-model layer)", () => {
-  it("should reflect changes in filters in all applicable views", async () => {
-    // create query client for test
-    const queryClient = createQueryClient();
+import rootViewModelDefaultDependencies from "./Root.view-model.dependencies";
 
+describe("Root Integration (view-model layer services)", () => {
+  it("should reflect changes in filters in all applicable views", async () => {
     // create mock tasks
     const mockTasks: Task[] = [
       { id: "1", text: "Task 1", ownerId: "user-1" },
@@ -29,44 +27,42 @@ describe("Root Integration (view-model layer)", () => {
       { id: "4", text: "Task 4", ownerId: "user-2" },
     ];
 
+    // create mock tasks service
+    const tasksService: ITasksService = {
+      addTask: vi.fn(async () => ({
+        id: "1",
+        text: "task",
+        ownerId: "user-1",
+      })),
+      deleteTask: vi.fn(),
+      listTasks: vi.fn(async () => mockTasks),
+    };
+
     // create mock users
     const mockUsers: User[] = [
       { id: "user-1", name: "User 1", profileImageUrl: "./src/user-1" },
       { id: "user-2", name: "User 2", profileImageUrl: "./src/user-2" },
     ];
 
-    // create mock commands
-    const commands: CommandsContextInterface = {
-      addTaskCommand: async () => ({
-        id: "1",
-        text: "task",
-        ownerId: "user-1",
-      }),
-      listTasksCommand: async () => mockTasks,
-      deleteTaskCommand: async () => {},
-      getUserCommand: async (userId) => {
+    // create mock users service
+    const usersService: IUsersService = {
+      getUserById: vi.fn(async (userId) => {
         return mockUsers.find((user) => user.id === userId);
-      },
-      listUsersCommand: async () => mockUsers,
+      }),
+      listUsers: vi.fn(async () => mockUsers),
     };
 
-    // create root view model dependencies
+    // create root dependencies
     const rootViewModelDependencies: RootViewModelDependencies = {
-      createAddTaskCommand: () => commands.addTaskCommand,
-      createDeleteTaskCommand: () => commands.deleteTaskCommand,
-      createGetUserCommand: () => commands.getUserCommand,
-      createListTasksCommand: () => commands.listTasksCommand,
-      createListUsersCommand: () => commands.listUsersCommand,
-      createQueryClient: () => queryClient,
-      createTasksService: () => ({}) as ITasksService,
-      createUsersService: () => ({}) as IUsersService,
+      ...rootViewModelDefaultDependencies,
+      createTasksService: () => tasksService,
+      createUsersService: () => usersService,
     };
 
     /**
      * Create provider tree
      */
     const Providers = createProviderTree([
-      <QueryClientProvider client={queryClient} />,
       <RootViewModelContext.Provider value={rootViewModelDependencies} />,
     ]);
 
@@ -129,7 +125,119 @@ describe("Root Integration (view-model layer)", () => {
   });
 });
 
-describe("Root Integration (view layer)", () => {
+describe("Root Integration (view-model layer commands)", () => {
+  it("should reflect changes in filters in all applicable views", async () => {
+    // create query client for test
+    const queryClient = createQueryClient();
+
+    // create mock tasks
+    const mockTasks: Task[] = [
+      { id: "1", text: "Task 1", ownerId: "user-1" },
+      { id: "2", text: "Task 2", ownerId: "user-1" },
+      { id: "3", text: "Task 3", ownerId: "user-2" },
+      { id: "4", text: "Task 4", ownerId: "user-2" },
+    ];
+
+    // create mock users
+    const mockUsers: User[] = [
+      { id: "user-1", name: "User 1", profileImageUrl: "./src/user-1" },
+      { id: "user-2", name: "User 2", profileImageUrl: "./src/user-2" },
+    ];
+
+    // create mock commands
+    const commands: CommandsContextInterface = {
+      addTaskCommand: async () => ({
+        id: "1",
+        text: "task",
+        ownerId: "user-1",
+      }),
+      listTasksCommand: async () => mockTasks,
+      deleteTaskCommand: async () => {},
+      getUserCommand: async (userId) => {
+        return mockUsers.find((user) => user.id === userId);
+      },
+      listUsersCommand: async () => mockUsers,
+    };
+
+    // create root view model dependencies
+    const rootViewModelDependencies: RootViewModelDependencies = {
+      createAddTaskCommand: () => commands.addTaskCommand,
+      createDeleteTaskCommand: () => commands.deleteTaskCommand,
+      createGetUserCommand: () => commands.getUserCommand,
+      createListTasksCommand: () => commands.listTasksCommand,
+      createListUsersCommand: () => commands.listUsersCommand,
+      createQueryClient: () => queryClient,
+      createTasksService: () => ({}) as ITasksService,
+      createUsersService: () => ({}) as IUsersService,
+    };
+
+    /**
+     * Create provider tree
+     */
+    const Providers = createProviderTree([
+      <RootViewModelContext.Provider value={rootViewModelDependencies} />,
+    ]);
+
+    /**
+     * Render a version that injects all the dependencies
+     * we created further up so that we can test our integration
+     */
+    render(
+      <Providers>
+        <Root />
+      </Providers>,
+    );
+
+    // wait for loading to finish
+    await waitFor(() =>
+      expect(screen.queryByText(/Loading/)).not.toBeInTheDocument(),
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 4/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 1",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 2/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 2",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 2/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "All",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 4/)).toBeInTheDocument();
+  });
+});
+
+describe("Root Integration (view layer commands)", () => {
   it("should reflect changes in filters in all applicable views", async () => {
     // create query client for test
     const queryClient = createQueryClient();
@@ -176,7 +284,6 @@ describe("Root Integration (view layer)", () => {
      * Create provider tree
      */
     const Providers = createProviderTree([
-      <QueryClientProvider client={queryClient} />,
       <RootContext.Provider value={rootDependencies} />,
     ]);
 
--- src/features/signals-mvvm-commands/TasksPropDrillingDiContainersLayered/views/Root/Root.view.integration.test.tsx
+++ src/features/signals-mvvm-commands/TasksPropDrillingDiContainersLayered/views/Root/Root.view.integration.test.tsx
@@ -11,7 +11,112 @@ import { ITasksService } from "../../services/tasks.service";
 import { IUsersService } from "../../services/users.service";
 import { createQueryClient } from "../../utils/create-query-client";
 
-describe("Root Integration (view-model layer)", () => {
+import rootViewModelDefaultDependencies from "./Root.view-model.dependencies";
+
+describe("Root Integration (view-model layer services)", () => {
+  it("should reflect changes in filters in all applicable views", async () => {
+    // create mock tasks
+    const mockTasks: Task[] = [
+      { id: "1", text: "Task 1", ownerId: "user-1" },
+      { id: "2", text: "Task 2", ownerId: "user-1" },
+      { id: "3", text: "Task 3", ownerId: "user-2" },
+      { id: "4", text: "Task 4", ownerId: "user-2" },
+    ];
+
+    // create mock tasks service
+    const tasksService: ITasksService = {
+      addTask: vi.fn(async () => ({
+        id: "1",
+        text: "task",
+        ownerId: "user-1",
+      })),
+      deleteTask: vi.fn(),
+      listTasks: vi.fn(async () => mockTasks),
+    };
+
+    // create mock users
+    const mockUsers: User[] = [
+      { id: "user-1", name: "User 1", profileImageUrl: "./src/user-1" },
+      { id: "user-2", name: "User 2", profileImageUrl: "./src/user-2" },
+    ];
+
+    // create mock users service
+    const usersService: IUsersService = {
+      getUserById: vi.fn(async (userId) => {
+        return mockUsers.find((user) => user.id === userId);
+      }),
+      listUsers: vi.fn(async () => mockUsers),
+    };
+
+    // create root dependencies
+    const rootDependencies: RootDependencies = {
+      App: defaultDependencies.App,
+      useRootViewModel: () =>
+        defaultDependencies.useRootViewModel({
+          dependencies: {
+            ...rootViewModelDefaultDependencies,
+            createTasksService: () => tasksService,
+            createUsersService: () => usersService,
+          },
+        }),
+    };
+
+    /**
+     * Render a version that injects all the dependencies
+     * we created further up so that we can test our integration
+     */
+    render(<Root dependencies={rootDependencies} />);
+
+    // wait for loading to finish
+    await waitFor(() =>
+      expect(screen.queryByText(/Loading/)).not.toBeInTheDocument(),
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 4/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 1",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 2/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 2",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 2/)).toBeInTheDocument();
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "All",
+    );
+
+    // assert
+    expect(screen.getByText(/Task 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 2/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 3/)).toBeInTheDocument();
+    expect(screen.getByText(/Task 4/)).toBeInTheDocument();
+    expect(screen.getByText(/Count: 4/)).toBeInTheDocument();
+  });
+});
+
+describe("Root Integration (view-model layer commands)", () => {
   it("should reflect changes in filters in all applicable views", async () => {
     // create query client for test
     const queryClient = createQueryClient();
@@ -118,7 +223,7 @@ describe("Root Integration (view-model layer)", () => {
   });
 });
 
-describe("Root Integration (view layer)", () => {
+describe("Root Integration (view layer commands)", () => {
   it("should reflect changes in filters in all applicable views", async () => {
     // create query client for test
     const queryClient = createQueryClient();
