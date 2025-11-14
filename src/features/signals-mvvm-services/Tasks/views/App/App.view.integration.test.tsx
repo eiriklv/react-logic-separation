8852548f2309ca58f422bb63bcb1e87b8be7c241
Eirik L. Vullum <evullum@gmail.com>
2025-11-14 08:59:30 +0100

    Add more integration tests

--- src/features/signals-mvvm-services/Tasks/views/App/App.view.integration.test.tsx
+++ src/features/signals-mvvm-services/Tasks/views/App/App.view.integration.test.tsx
@@ -2,16 +2,33 @@ import { render, screen } from "@testing-library/react";
 import { App } from "./App.view";
 import defaultDependencies, { AppDependencies } from "./App.view.dependencies";
 import { ComponentProps } from "react";
+import { Task, User } from "../../types";
+import userEvent from "@testing-library/user-event";
 
 describe("App", () => {
-  it("Renders correctly", () => {
+  it("Renders correctly", async () => {
     // arrange
+    const mockTasks: Task[] = [
+      { id: "1", text: "Task 1", ownerId: "user-1" },
+      { id: "2", text: "Task 2", ownerId: "user-1" },
+      { id: "3", text: "Task 3", ownerId: "user-2" },
+      { id: "4", text: "Task 4", ownerId: "user-2" },
+    ];
+
+    const mockUsers: User[] = [
+      { id: "user-1", name: "User 1", profileImageUrl: "./src/user-1" },
+      { id: "user-2", name: "User 2", profileImageUrl: "./src/user-2" },
+    ];
+
+    const addTask = vi.fn();
+    const setSelectedOwnerId = vi.fn();
+
     const actionsDependencies: ComponentProps<
       AppDependencies["Actions"]
     >["dependencies"] = {
       useActionsViewModel: () => ({
-        addTask: vi.fn(),
-        users: [],
+        addTask,
+        users: mockUsers,
       }),
     };
 
@@ -20,8 +37,8 @@ describe("App", () => {
     >["dependencies"] = {
       useFiltersViewModel: () => ({
         selectedOwnerId: "",
-        setSelectedOwnerId: vi.fn(),
-        users: [],
+        setSelectedOwnerId,
+        users: mockUsers,
       }),
     };
 
@@ -33,8 +50,8 @@ describe("App", () => {
         isFetching: false,
         isLoading: false,
         isSaving: false,
-        tasks: [],
-        tasksCount: 0,
+        tasks: mockTasks,
+        tasksCount: mockTasks.length,
       }),
     };
 
@@ -56,5 +73,35 @@ describe("App", () => {
     expect(screen.getByText(/Actions/)).toBeInTheDocument();
     expect(screen.getByText(/Filters/)).toBeInTheDocument();
     expect(screen.getByText(/Tasks/)).toBeInTheDocument();
+
+    // assert
+    expect(screen.getAllByTestId("TaskItem")).toHaveLength(4);
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 1",
+    );
+
+    // assert
+    expect(setSelectedOwnerId).toHaveBeenCalledWith("user-1");
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "User 2",
+    );
+
+    // assert
+    expect(setSelectedOwnerId).toHaveBeenCalledWith("user-2");
+
+    // act
+    await userEvent.selectOptions(
+      screen.getByRole("combobox", { name: "Owner:" }),
+      "All",
+    );
+
+    // assert
+    expect(setSelectedOwnerId).toHaveBeenCalledWith("");
   });
 });
