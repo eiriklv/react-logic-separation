a128b70201ac02ade609f86ce298a56cee212cdf
Eirik L. Vullum <evullum@gmail.com>
2025-12-31 23:19:58 +0100

    Comments

--- src/features/signals-mvvm-services/Tasks/providers/services.ts
+++ src/features/signals-mvvm-services/Tasks/providers/services.ts
@@ -2,15 +2,17 @@ import { useContext } from "react";
 import { ServicesContext } from "./services.context";
 
 /**
- * The purpose of this context is to be able to
- * share models throughout the application,
- * and that any layer can be the provider of
- * those models.
+ * The purpose of this hook is to be able to
+ * consume services throughout the application.
  *
- * The consumers of the models should not need
+ * The consumers of the services should not need
  * to care about where the models were provided,
  * just that they are available
+ *
+ * The consumer should not have to know that
+ * a context exists - that's just an implementation detail
  */
+
 export const useServices = () => {
   const services = useContext(ServicesContext);
 
