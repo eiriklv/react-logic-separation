9751974ae70fbf592e81119d7ba43928d88efec6
Eirik Vullum <evullum@gmail.com>
2026-04-12 00:17:24 +0200

    Add minor optimization

--- src/features/solid-signals/Timer/solid-signals-utils.ts
+++ src/features/solid-signals/Timer/solid-signals-utils.ts
@@ -1,22 +1,41 @@
 import { Accessor, createEffect, createRoot, untrack } from "@solidjs/signals";
 import { useMemo, useSyncExternalStore } from "react";
 
-function createSignalSubscriber<T>(signal: Accessor<T>): (callback: () => void) => () => void {
+function createSignalSubscriber<T>(
+  signal: Accessor<T>,
+): (callback: () => void) => () => void {
   return (callback: () => void) => {
+    let isInitial = true;
+
     let dispose: () => void = () => {
-      console.warn('attempting to disposed before disposer has been assigned');
+      console.warn("attempting to disposed before disposer has been assigned");
     };
-    
+
     createRoot((disposer) => {
       dispose = disposer;
-      createEffect(() => signal(), () => callback());
-    })
+      createEffect(
+        () => signal(),
+        () => {
+          /**
+           * Effects are always called on creation,
+           * but in this case we only want to
+           * trigger the callback on changes
+           */
+          if (isInitial) {
+            isInitial = false;
+            return;
+          }
+
+          callback();
+        },
+      );
+    });
 
     return dispose;
-  }
+  };
 }
 
-function createSignalPeeker <T>(signal: Accessor<T>) {
+function createSignalPeeker<T>(signal: Accessor<T>) {
   return () => untrack(() => signal());
 }
 
@@ -27,8 +46,5 @@ export const useSignalValue = <T>(signal: Accessor<T>) => {
   const subscribe = useMemo(() => createSignalSubscriber(signal), [signal]);
   const peek = useMemo(() => createSignalPeeker(signal), [signal]);
 
-  return useSyncExternalStore(
-    subscribe,
-    peek,
-  );
+  return useSyncExternalStore(subscribe, peek);
 };
